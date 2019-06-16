e5b38beeb52c4ca9eacfc24ac557a29655cf7a4d
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-06-16 13:01:03 +0200

    Add Size to Forecase Icons

--- lib/widgets/WeatherConditions.dart
+++ lib/widgets/WeatherConditions.dart
@@ -16,25 +16,26 @@ class WeatherConditions extends StatelessWidget {
       case WeatherCondition.snow:
       case WeatherCondition.sleet:
       case WeatherCondition.hail:
-        image = Image.asset('assets/forecast-icons/snow.png');
+        image = Image.asset('assets/forecast-icons/snow.png', height: 80, width: 80);
         break;
 
       case WeatherCondition.thunderstorm:
-        image = Image.asset('assets/forecast-icons/storm.png');
+        image = Image.asset('assets/forecast-icons/storm.png', height: 80, width: 80);
 
         break;
       case WeatherCondition.heavyRain:
       case WeatherCondition.lightRain:
       case WeatherCondition.showers:
-        image = Image.asset('assets/forecast-icons/rain.png');
+        image = Image.asset('assets/forecast-icons/rain.png', height: 80, width: 80);
         break;
       case WeatherCondition.heavyCloud:
-        image = Image.asset('assets/forecast-icons/cloudy.png');
+        image = Image.asset('assets/forecast-icons/cloudy.png', height: 80, width: 80);
         break;
       case WeatherCondition.lightCloud:
       case WeatherCondition.clear:
       case WeatherCondition.unknown:
-        image = Image.asset('assets/forecast-icons/sun.png');
+        image =
+            Image.asset('assets/forecast-icons/sun.png', height: 80, width: 80);
         break;
     }
     return image;
